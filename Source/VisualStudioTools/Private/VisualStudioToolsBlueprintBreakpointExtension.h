52056ef84f2be1442701dc9891c068937059df3d
Guilherme Íscaro <cabelitostos@gmail.com>
2024-10-30 14:07:19 -0300

    Add support for setting breakpoints from the unreal editor in Native methods (#51)
    * Add support for setting breakpoints from the unreal editor in Native methods
    
    * review fixes
    
    * review fixes

---
+++ Source/VisualStudioTools/Private/VisualStudioToolsBlueprintBreakpointExtension.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <CoreMinimal.h>
+#include <EditorSubsystem.h>
+#include <EdGraph/EdGraph.h>
+#include <EdGraph/EdGraphNode.h>
+#include <EdGraph/EdGraphPin.h>
+#include <GraphEditorModule.h>
+#include <VisualStudioDTE.h>
+#include <Microsoft/COMPointer.h>
+#include "VisualStudioToolsBlueprintBreakpointExtension.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogUVisualStudioToolsBlueprintBreakpointExtension, Log, All);
+
+UCLASS()
+class UVisualStudioToolsBlueprintBreakpointExtension : public UEditorSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	DECLARE_MULTICAST_DELEGATE_ThreeParams(FOnNodeMenuExtensionHookRequestDelegate, const UEdGraphNode*, const UEdGraph*, TSet<FName>&);
+
+	void Initialize(FSubsystemCollectionBase& Collection) override;
+	void Deinitialize() override;
+
+	FOnNodeMenuExtensionHookRequestDelegate& OnNodeMenuExtensionHookRequest() { return OnNodeMenuExtensionHookRequestDelegate; }
+
+private:
+	FOnNodeMenuExtensionHookRequestDelegate OnNodeMenuExtensionHookRequestDelegate;
+
+	TSharedRef<FExtender> HandleOnExtendGraphEditorContextMenu(
+		const TSharedRef<FUICommandList> CommandList,
+		const UEdGraph* Graph,
+		const UEdGraphNode* Node,
+		const UEdGraphPin* Pin,
+		bool bIsConst);
+
+	void AddVisualStudioBlueprintBreakpointMenuOption(FMenuBuilder& MenuBuilder, const UEdGraphNode* node);
+
+	void AddVisualStudioBreakpoint(const UEdGraphNode* Node);
+
+	bool GetFunctionDefinitionLocation(const UEdGraphNode* Node, FString& SourceFilePath, FString& SymbolName, uint32& SourceLineNumber);
+
+	bool SetVisualStudioBreakpoint(const UEdGraphNode* Node, const FString& SourceFilePath, const FString& SymbolName, uint32 SourceLineNumber);
+
+	bool CanAddVisualStudioBreakpoint(const UEdGraphNode* Node, UClass** OutOwnerClass, UFunction** OutFunction);
+
+	void ShowOperationResultNotification(bool bBreakpointAdded, const FString& SymbolName);
+
+	FString GetProjectPath(const FString& ProjectDir);
+
+	bool GetRunningVisualStudioDTE(TComPtr<EnvDTE::_DTE>& OutDTE);
+};
